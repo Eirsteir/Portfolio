d891017c0a76b9e101738e428d82eb078abcd2ef
Eirik Steira <steiraeirik@gmail.com>
2023-08-23 16:33:27 +0200

    Add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,16 @@ import { api } from "~/utils/api";
 import "~/styles/globals.css";
 import "@fontsource-variable/cabin";
 import Layout from "~/components/layout";
+import { Analytics } from "@vercel/analytics/react";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+      <Analytics />
+    </>
   );
 };
 
